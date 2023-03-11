cb94de8ac9ed2af01f304dca0fcb8d900744faf8
Alef Soares <alefcard.soares@gmail.com>
2023-03-11 15:47:43 -0300

    improvements on code

--- main.js
+++ main.js
@@ -1,6 +1,66 @@
-const button = document.querySelector("#gestante")
-const modal = document.querySelector(".imagensGestantes")
+const buttonPortifolio = document.querySelector("#portifolio")
 
-button.onclick = function () {
-    modal.classList.add("hidden")
+// buttonPortifolio.addEventListener()
+
+
+/*----------------------------------------------------------------------------*/
+
+const buttonTodos = document.querySelector("#todos")
+const buttonGestante = document.querySelector("#gestante")
+const buttonNewborn = document.querySelector("#newborn")
+const buttonAcompanhamento = document.querySelector("#acompanhamento")
+const buttonSmash = document.querySelector("#smashTheCake")
+const buttonPreWedding = document.querySelector("#preWedding")
+
+const fotosTodos = document.querySelector(".imagensTodos")
+const fotosGestante = document.querySelector(".imagensGestantes")
+const fotosNewborn = document.querySelector(".imagensNewborn")
+const fotosAcompanhamento = document.querySelector(".imagensAcompanhamento")
+const fotosSmash = document.querySelector(".imagensSmash")
+const fotosPreWedding = document.querySelector(".preWedding")
+
+buttonGestante.onclick = function () {
+    fotosGestante.classList.remove("active")
+    fotosAcompanhamento.classList.add("active")
+    fotosNewborn.classList.add("active")
+    fotosSmash.classList.add("active")
+    fotosPreWedding.classList.add("active")
+    window.scroll(0,1450)
 }
+ 
+buttonNewborn.onclick = function () {
+    fotosNewborn.classList.remove("active")
+    fotosGestante.classList.add("active")
+    fotosSmash.classList.add("active")
+    fotosAcompanhamento.classList.add("active")
+    fotosPreWedding.classList.add("active")
+    window.scroll(0,1340)
+}
+
+buttonAcompanhamento.onclick = function () {
+    fotosAcompanhamento.classList.remove("active")
+    fotosGestante.classList.add("active")
+    fotosNewborn.classList.add("active")
+    fotosSmash.classList.add("active")
+    fotosPreWedding.classList.add("active")
+    window.scroll(0,1340)
+}
+
+buttonSmash.onclick = function () {
+    fotosSmash.classList.remove("active")
+    fotosAcompanhamento.classList.add("active")
+    fotosGestante.classList.add("active")
+    fotosNewborn.classList.add("active")
+    fotosPreWedding.classList.add("active")
+    window.scroll(0,1340)
+}
+
+buttonPreWedding.onclick = function () {
+    fotosPreWedding.classList.remove("active")
+    fotosSmash.classList.add("active")
+    fotosAcompanhamento.classList.add("active")
+    fotosGestante.classList.add("active")
+    fotosNewborn.classList.add("active")
+    window.scroll(0,1340)
+}
+
