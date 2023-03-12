7fd30ef7e607e9a11096a80fa1eaee14abb5ae8e
Alef Soares <alefcard.soares@gmail.com>
2023-03-11 22:08:55 -0300

    improvements on code

--- main.js
+++ main.js
@@ -1,66 +1,81 @@
-const buttonPortifolio = document.querySelector("#portifolio")
+//HAMBURGER
+const burger = document.querySelector('.burger')
+const navUl = document.querySelector('.nav-links')
 
-// buttonPortifolio.addEventListener()
-
+burger.addEventListener('click', function(){
+  burger.classList.toggle('activer')
+  navUl.classList.toggle('activer')
+})
 
 /*----------------------------------------------------------------------------*/
 
-const buttonTodos = document.querySelector("#todos")
-const buttonGestante = document.querySelector("#gestante")
-const buttonNewborn = document.querySelector("#newborn")
-const buttonAcompanhamento = document.querySelector("#acompanhamento")
-const buttonSmash = document.querySelector("#smashTheCake")
-const buttonPreWedding = document.querySelector("#preWedding")
+const buttonTodos = document.querySelector("#todos");
+const buttonGestante = document.querySelector("#gestante");
+const buttonNewborn = document.querySelector("#newborn");
+const buttonAcompanhamento = document.querySelector("#acompanhamento");
+const buttonSmash = document.querySelector("#smashTheCake");
+const buttonPreWedding = document.querySelector("#preWedding");
 
-const fotosTodos = document.querySelector(".imagensTodos")
-const fotosGestante = document.querySelector(".imagensGestantes")
-const fotosNewborn = document.querySelector(".imagensNewborn")
-const fotosAcompanhamento = document.querySelector(".imagensAcompanhamento")
-const fotosSmash = document.querySelector(".imagensSmash")
-const fotosPreWedding = document.querySelector(".preWedding")
+const fotosTodos = document.querySelector(".all");
+const fotosGestante = document.querySelector(".imagensGestantes");
+const fotosNewborn = document.querySelector(".imagensNewborn");
+const fotosAcompanhamento = document.querySelector(".imagensAcompanhamento");
+const fotosSmash = document.querySelector(".imagensSmash");
+const fotosPreWedding = document.querySelector(".preWedding");
+
 
 buttonGestante.onclick = function () {
-    fotosGestante.classList.remove("active")
-    fotosAcompanhamento.classList.add("active")
-    fotosNewborn.classList.add("active")
-    fotosSmash.classList.add("active")
-    fotosPreWedding.classList.add("active")
-    window.scroll(0,1450)
-}
- 
-buttonNewborn.onclick = function () {
-    fotosNewborn.classList.remove("active")
-    fotosGestante.classList.add("active")
-    fotosSmash.classList.add("active")
-    fotosAcompanhamento.classList.add("active")
-    fotosPreWedding.classList.add("active")
-    window.scroll(0,1340)
+    fotosGestante.classList.toggle("active") 
+
+    var elements = [fotosAcompanhamento, 
+    fotosNewborn, fotosSmash,fotosPreWedding];
+
+    elements.forEach(function(i) {
+        i.classList.add("active")
+    })
 }
 
 buttonAcompanhamento.onclick = function () {
-    fotosAcompanhamento.classList.remove("active")
-    fotosGestante.classList.add("active")
-    fotosNewborn.classList.add("active")
-    fotosSmash.classList.add("active")
-    fotosPreWedding.classList.add("active")
-    window.scroll(0,1340)
+    fotosAcompanhamento.classList.toggle("active") 
+
+    var elements = [ fotosGestante, 
+    fotosNewborn, fotosSmash,fotosPreWedding];
+
+    elements.forEach(function(i) {
+        i.classList.add("active")
+    })
 }
 
+buttonNewborn.onclick = function () {
+    fotosNewborn.classList.toggle("active") 
+
+    var elements = [fotosGestante,fotosAcompanhamento, 
+    fotosSmash,fotosPreWedding];
+
+    elements.forEach(function(i) {
+        i.classList.add("active")
+    })
+}
+
+
 buttonSmash.onclick = function () {
-    fotosSmash.classList.remove("active")
-    fotosAcompanhamento.classList.add("active")
-    fotosGestante.classList.add("active")
-    fotosNewborn.classList.add("active")
-    fotosPreWedding.classList.add("active")
-    window.scroll(0,1340)
+    fotosSmash.classList.toggle("active") 
+
+    var elements = [fotosGestante,fotosAcompanhamento, fotosNewborn, fotosPreWedding];
+
+    elements.forEach(function(i) {
+        i.classList.add("active")
+    })
 }
 
 buttonPreWedding.onclick = function () {
-    fotosPreWedding.classList.remove("active")
-    fotosSmash.classList.add("active")
-    fotosAcompanhamento.classList.add("active")
-    fotosGestante.classList.add("active")
-    fotosNewborn.classList.add("active")
-    window.scroll(0,1340)
-}
+    fotosPreWedding.classList.toggle("active") 
 
+    var elements = [fotosGestante,fotosAcompanhamento,
+        fotosNewborn, fotosSmash];
+
+    elements.forEach(function(i) {
+        i.classList.add("active")
+    })
+
+}
